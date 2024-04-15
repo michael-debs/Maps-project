1f086759fefc8da4f9c3cc339fdf09321a09e2cc
Michael Debs <michaeldebs16@gmail.com>
2024-04-15 14:09:26 -0700

    BugFix: authRouter
    registered authRouter to index.js

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 // routes
 const userRouter = require("./userRoutes")
+const authRouter = require("./authRoutes")
 
 router.use("/user", userRouter)
+router.use("/auth", authRouter)
 
 module.exports = router
