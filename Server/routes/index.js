4fa33c47893227d1942dbb085d18e08e3282fd37
Michael Debs <michaeldebs16@gmail.com>
2024-04-25 21:12:53 +0300

    Add activity backend

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 // routes
 const userRouter = require("./userRoutes")
 const authRouter = require("./authRoutes")
+const activityRouter = require("./activityRoutes")
 
 router.use("/user", userRouter)
 router.use("/auth", authRouter)
+router.use("/activity", activityRouter)
 
 module.exports = router
