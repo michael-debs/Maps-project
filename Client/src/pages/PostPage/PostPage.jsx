294469b92ba665c4bf1e402509f273e55f5c94da
Riman <rimanxminawi@gmail.com>
2024-05-29 23:40:54 +0300

    add logic to the post, install fortawesome to use FontAwesomeIcon for the like and add style to it

--- Client/src/pages/PostPage/PostPage.jsx
+++ Client/src/pages/PostPage/PostPage.jsx
@@ -1,43 +1,79 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import usePost from "../../hooks/usePost";
-import defaultProfile from "../../assets/images/defaultProfile.png";
+import defaultActivity from "../../assets/images/defaultActivity.png";
 import styles from "./PostPage.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function PostPage() {
   const { id } = useParams();
   const { post } = usePost(id);
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  useEffect(() => {
+    if (post) {
+      setLikeCount(post.likes);
+    }
+  }, [post]);
+
+  const handleLikeClick = () => {
+    setIsLiked((prevIsLiked) => !prevIsLiked);
+    setLikeCount((prevLikeCount) => prevLikeCount + (isLiked ? -1 : 1));
+  };
+
+  if (!post) {
+    return <div>No post found</div>;
+  }
+
+  const activityProfile = post.activity?.profile || defaultActivity;
+  const activityName = post.activity?.name || "Unknown Activity";
+  const userName = post.user
+    ? `${post.user.firstName} ${post.user.lastName}`
+    : "Unknown User";
+
+  const formattedDate = post.createdAt
+    ? new Date(post.createdAt).toLocaleDateString()
+    : "Unknown Date";
+
+  console.log({ post });
 
   return (
     <div className={styles.body}>
       <div className={styles.container}>
         <header className={styles.header}>
           <div className={styles.picandname}>
-            <img className={styles.Picture} src={defaultProfile} alt="User" />
-            <div className={styles.Name}>Riman Minawi</div>
+            <img
+              className={styles.Picture}
+              src={activityProfile}
+              alt="Activity"
+            />
+            <div className={styles.Name}>
+              <div className={styles.ActivityName}>{activityName}</div>
+              <div className={styles.UserName}>{userName}</div>
+            </div>
           </div>
-          <div className={styles.datePosted}>1.11.2024</div>
+          <div className={styles.datePosted}>{formattedDate}</div>
         </header>
 
         <section className={styles.titleSection}>
-          {/* Post title */}
-          <h2 className={styles.title}>Why I Love Winter</h2>
+          <h2 className={styles.title}>{post.title}</h2>
         </section>
         <section className={styles.contentSection}>
-          {/* Post content */}
-          <p className={styles.content}>
-            I like winter because it is a wonderful season. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Integer sit amet leo nec
-            justo ullamcorper ultricies. Nullam rhoncus odio at interdum
-            tristique. Aenean aliquam condimentum felis, sit amet consectetur
-            turpis pellentesque vel. Aliquam ut vehicula sem. Suspendisse vitae
-            suscipit nisl. Fusce vestibulum, elit nec feugiat fermentum, lectus
-            justo scelerisque ligula, eget molestie orci risus sed nunc. Donec
-            eget neque nec nisl ultrices consequat. Vestibulum et nunc
-            ultricies, dignissim arcu sit amet, gravida sapien. Vivamus
-            malesuada magna a fermentum pharetra. Nulla facilisi. Fusce aliquet
-            sapien id ex varius suscipit. Quisque nec sem erat. Cras maximus
-            massa vitae finibus placerat. Phasellus vehicula orci ac libero
-            ultricies, eget accumsan velit ultricies.
+          <p className={styles.content}>{post.content}</p>
+        </section>
+
+        <section className={styles.likesComments}>
+          <p className={styles.likes}>
+            {likeCount}
+            <FontAwesomeIcon
+              icon={faHeart}
+              className={`${styles.likeIcon} ${isLiked ? styles.liked : ""}`}
+              onClick={handleLikeClick}
+              size="2x"
+            />
           </p>
         </section>
       </div>
